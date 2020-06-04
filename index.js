216af57ffddf39221917e22c5ce553906325cb7e
ajm1808 <ajm180885@gmail.com>
2020-06-04 18:27:15 +0100

    adjusted to get quotes to push to page

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express=require('express');
 const hbs=require('express-handlebars');
-const path =require('path');
+const path=require('path');
+const getPlaceholder=require('./lib/getplaceholder');
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
 app.engine('hbs',hbs({
-    extname:'.hbs',
     defaultLayout:'main',
-    layoutsDir:path.join(__dirname,'views','layouts'),
-    partialsDir:path.join(__dirname,'views','partials')
+    extname:'.hbs',
+    layoutsDir:path.join(__dirname,'views','layouts'),partialsDir:path.join(__dirname,'views','partials')
 }));
 
-app.set('view-engine','.hbs');
+app.set('view engine','hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get('/',async(req,res)=>{
+    let data=await getPlaceholder();
+    res.render('index', {data, listExists: true});
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+
+app.get('*',(req,res)=>{
+    res.render('404');
+});
+
+app.listen(3000,()=>{
+    console.log("Listening to port 3000")
 });
